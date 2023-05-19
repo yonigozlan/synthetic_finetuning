fixed quaternion to axis angle method

generate_dataset.py
@@ -59,6 +59,9 @@ class DatasetGenerator:
             }
         ]
         self.data_dict_test = deepcopy(self.data_dict_train)
+        self.total_source_images = 0
+        self.total_missing_bboxes = 0
+        self.total_error_reconstruction = 0
 
         if not os.path.exists(os.path.join(self.output_path_train, "images")):
             os.makedirs(os.path.join(self.output_path_train, "images"))
@@ -129,9 +132,17 @@ class DatasetGenerator:
                     ann,
                     groundtruth_landmarks,
                     coco_landmarks,
+                    success,
                 ) = self.get_grountruth_landmarks(video_scene, index_frame)
-                if "bbox" not in ann:
+
+                self.total_source_images += 1
+                if not success:
+                    self.total_error_reconstruction += 1
                     continue
+                if "bbox" not in ann:
+                    self.total_missing_bboxes
+                    continue
+
                 annotation_dict = self.generate_annotation_dict(ann, mode=mode)
                 annotation_dict["keypoints"] = groundtruth_landmarks
                 annotation_dict["coco_keypoints"] = coco_landmarks
@@ -151,6 +162,10 @@ class DatasetGenerator:
             encoding="utf-8",
         ) as f:
             json.dump(self.data_dict_test, f, ensure_ascii=False, indent=4)
+
+        print("total source images: ", self.total_source_images)
+        print("total missing bboxes: ", self.total_missing_bboxes)
+        print("total error reconstruction: ", self.total_error_reconstruction)
 
     def generate_annotation_dict(self, ann: dict, mode: str = "train"):
         annotation_dict = {}
@@ -179,6 +194,8 @@ class DatasetGenerator:
         projected_vertices = video_scene.compute_2d_projection(
             joints, augmented_vertices, method=self.method
         )
+        if np.isnan(projected_vertices).any():
+            return img, ann, {}, {}, False
         groundtruth_landmarks = {
             name: {"x": point[0], "y": point[1]}
             for name, point in zip(AUGMENTED_VERTICES_NAMES, projected_vertices)
@@ -196,7 +213,7 @@ class DatasetGenerator:
             else:
                 groundtruth_landmarks[name]["v"] = 1
 
-        return img, ann, groundtruth_landmarks, coco_landmarks
+        return img, ann, groundtruth_landmarks, coco_landmarks, True
 
 
 if __name__ == "__main__":

reconstruction.py
@@ -13,17 +13,22 @@ def get_axis_angle_from_ann(ann, start_index, end_index):
     ]
     axis_angle = []
     for quaternion in quaternions:
-        angle = 2 * np.arccos(quaternion[0])
+        norm = np.sqrt(quaternion[1] ** 2 + quaternion[2] ** 2 + quaternion[3] ** 2)
+        angle = 2 * np.arctan2(
+            norm,
+            quaternion[0],
+        )
         if angle == 0:
             axis_angle.append([0, 0, 0])
         else:
             axis_angle.append(
                 [
-                    angle * quaternion[1] / np.sqrt(1 - quaternion[0] * quaternion[0]),
-                    angle * quaternion[2] / np.sqrt(1 - quaternion[0] * quaternion[0]),
-                    angle * quaternion[3] / np.sqrt(1 - quaternion[0] * quaternion[0]),
+                    angle * quaternion[1] / norm,
+                    angle * quaternion[2] / norm,
+                    angle * quaternion[3] / norm,
                 ]
             )
+
     return torch.tensor(axis_angle, dtype=torch.float32).reshape([1, -1])
 
 
@@ -68,7 +73,6 @@ def get_smplx_model(
     betas,
     poses,
 ):
-
     return smplx.create(
         model_folder,
         model_type="smplx",
@@ -133,7 +137,6 @@ def show_mesh(
         nodes += [scene.add(joints_pcl, name="joints")]
 
     if plot_augmented_vertices:
-
         sm = trimesh.creation.uv_sphere(radius=0.01)
         sm.visual.vertex_colors = [0.1, 0.1, 0.9, 1.0]
         tfs = np.tile(np.eye(4), (len(augmented_vertices), 1, 1))